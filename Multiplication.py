eaa19385a2bef52d1946858408fe30fbd8b536e2
priya <priyaharinet@gmail.com>
2021-02-24 21:14:21 -0600

    multiplication test

---
+++ Multiplication.py
@@ -0,0 +1,7 @@
+# This code multiplies number1 and number2
+
+
+num1 = int(input('Enter the Number 1 '))
+num2 = int(input('Enter the Number 2 '))
+M = num1 * num2
+print("The value of",num1,"*" , num2,"is =", M)
